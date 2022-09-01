d3cf55cb6c8f083c35be72b306d4b25ee5854a88
Stanko Jevtic <bucajevtic2@hotmail.com>
2022-09-01 19:56:53 +0200

    Initial AddFirst unit test added.

---
+++ DoublyLinkedList.App.Tests/DoublyLinkedList/DoublyLinkedListTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using DoublyLinkedList.App.DoublyLinkedList;
+
+namespace DoublyLinkedList.App.Tests.DoublyLinkedList
+{
+    public class DoublyLinkedListTests
+    {
+        [Fact]
+        public void AddFirst_WhenListIsEmpty_Success()
+        {
+            var doublyLinkedList = new DoubleLinkedList();
+
+            doublyLinkedList.AddFirst(10);
+
+            Assert.Equal(doublyLinkedList.First.Data, 10);
+            Assert.Equal(doublyLinkedList.Last.Data, 10);
+        }
+    }
+}
+
