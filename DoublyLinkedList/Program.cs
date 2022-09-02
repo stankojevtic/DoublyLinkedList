fba2cc53510d81969d7708d690c4d4f04d73d793
Stanko Jevtic <bucajevtic2@hotmail.com>
2022-09-02 10:25:09 +0200

    Added new examples in Program.cs.

--- DoublyLinkedList/Program.cs
+++ DoublyLinkedList/Program.cs
@@ -17,13 +17,12 @@ public class Program
         var sortService = serviceProvider.GetService<ISortService>();
 
         //Example 1
+        Console.WriteLine("Example 1 - start");
         var doublyLinkedList = new DoubleLinkedList<Size>(sortService);
 
         doublyLinkedList.AddFirst(new Size(10));
-        doublyLinkedList.AddFirst(new Size(15));
         doublyLinkedList.AddFirst(new Size(20));
         doublyLinkedList.AddLast(new Size(25));
-        doublyLinkedList.Remove(new Size(15));
         doublyLinkedList.AddFirst(new Size(35));
         doublyLinkedList.AddLast(new Size(3));
 
@@ -41,9 +40,10 @@ public class Program
             Console.Write(node.Data + " ");
         }
 
-        Console.WriteLine();
+        Console.WriteLine("\nExample 1 - end");
 
         //Example 2
+        Console.WriteLine("Example 2 - start");
         var doublyLinkedList1 = new DoubleLinkedList<int>(sortService);
 
         doublyLinkedList1.AddFirst(10);
@@ -67,5 +67,34 @@ public class Program
         {
             Console.Write(node.Data + " ");
         }
+
+        Console.WriteLine("\nExample 2 - end");
+
+        //Example 3
+        Console.WriteLine("Example 3 - start");
+        var doublyLinkedList2 = new DoubleLinkedList<string>(sortService);
+
+        doublyLinkedList2.AddFirst("10");
+        doublyLinkedList2.AddFirst("15");
+        doublyLinkedList2.AddFirst("20");
+        doublyLinkedList2.AddLast("25");
+        doublyLinkedList2.Remove("15");
+        doublyLinkedList2.AddFirst("35");
+        doublyLinkedList2.AddLast("3");
+
+        foreach (var node in doublyLinkedList2)
+        {
+            Console.Write(node.Data + " ");
+        }
+
+        Console.WriteLine();
+
+        doublyLinkedList2.Sort();
+
+        foreach (var node in doublyLinkedList2)
+        {
+            Console.Write(node.Data + " ");
+        }
+        Console.WriteLine("\nExample 3 - end");
     }
 }
